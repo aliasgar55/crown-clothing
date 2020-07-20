3df281f86847943ee0d77298c6f4277ae943b72c
root <joancena1268@.com>
2020-07-20 23:53:13 +0530

    Added reslect created Cart Selector and replaced it with momolized selectors also updated ids in shop data

--- src/components/cart-icon/cart-icon-component.jsx
+++ src/components/cart-icon/cart-icon-component.jsx
@@ -3,18 +3,21 @@ import { connect } from "react-redux";
  
 import { ReactComponent as ShoppingIcon } from "../../assets/cart.svg"
 import { toggelCartHidden } from "../../redux/cart/cart-action.js"
+import { selectCartItemsCount } from "../../redux/cart/cart.selector"
 
 import "./cart-icon-styles.scss"
 
-const CartIcon = ({ toggelCartHidden }) => (
-<div className="cart-icon">
-	<ShoppingIcon className="shopping-icon" onClick={toggelCartHidden}/>
-	<span className="item-count">0</span>
+const CartIcon = ({ toggelCartHidden, itemCount }) => (
+<div className="cart-icon" onClick={toggelCartHidden} >
+	<ShoppingIcon className="shopping-icon" />
+	<span className="item-count">{itemCount}</span>
 </div>
 );
-
+const mapStateToProps = (state) => ({
+	itemCount : selectCartItemsCount(state)
+})
 const mapDispatchToProps = dispatch => ({
 	toggelCartHidden: () => dispatch(toggelCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
